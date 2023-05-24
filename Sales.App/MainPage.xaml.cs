1380a54d17810aa1fe7da33cd394b8402f8ecf11
jmezquita <jmgysoft@gmail.com>
2023-05-24 18:06:15 +0200

    add DbContext And conection

---
+++ Sales.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Sales.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Sales.Shared/Entities/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Shared.Entities
+{
+    public abstract class EntityBase
+    {
+        /// <summary>
+        /// Item Id
+        /// </summary>
+        public int Id { get; set; } 
+    }
+}
